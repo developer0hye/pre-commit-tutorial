b05c2bd67d5a62a724a886ccc0814a015d49812d
Yonghye Kwon <developer.0hye@gmail.com>
2023-03-06 11:13:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 a = 1 + 3
 b = 2 + 3
-c = a +                                 b
+c = a + b
 
 
-def black(f,                                       g, b, c, d, e):
+def black(f, g, b, c, d, e):
     pass
